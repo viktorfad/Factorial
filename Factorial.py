46e586546e6a695603646f91495542ef6495926c
viktorfad <66686633+viktorfad@users.noreply.github.com>
2020-07-01 16:55:11 +0300

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -2,12 +2,12 @@ import math
 
 def get_zeros(n):
     try:
-        k=0
+        k = 0
         for i in str(math.factorial(n))[::-1]:
-            if i=='0':
-                k+=1
-            else:
+            if i == '0':
+                k += 1
+            else :
                 return k
     except ValueError as e:
-        print('Ошибка! \nЧисло, факториал которого необходимо вычислить, должно быть неотрицательным и интегральным! \nВы передали: n={}'.format(n)) 
+        print('Ошибка! \nЧисло, факториал которого необходимо вычислить, должно быть неотрицательным и интегральным! \nВы передали: n={}'.format(n))
         return
