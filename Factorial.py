b349c4a0692c449d8530755e2f31186b0b6bdb90
Viktor <vityafad@gmail.com>
2020-07-01 15:57:19 +0300

    commit

---
+++ Factorial.py
@@ -0,0 +1,13 @@
+import math
+
+def get_zeros(n):
+    try:
+        k=0
+        for i in str(math.factorial(n))[::-1]:
+            if i=='0':
+                k+=1
+            else:
+                return k
+    except ValueError as e:
+        print('Ошибка! \nЧисло, факториал которого необходимо вычислить, должно быть неотрицательным и интегральным! \nВы передали: n={}'.format(n)) 
+        return
